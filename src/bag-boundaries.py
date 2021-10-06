3e5532182b5f15cbf17cd69b112efcf3814d19db
NicoMandel <nicolasjohann.mandel@hdr.qut.edu.au>
2021-10-06 12:22:57 +1000

    finding out bag boundaries for Qt

--- src/bag-boundaries.py
+++ src/bag-boundaries.py
@@ -10,17 +10,11 @@ import rosbag
 from datetime import datetime
 from tqdm import tqdm
 
-import quadtree as qt
-
 from argparse import ArgumentParser
 
-def decode_intensity(pts, intensity):
-        pt_dict = {}
-        for i, ch in enumerate(intensity):
-            pt = pts[i]
-            pt_dict[tuple([pt.x, pt.y])] = int(ch)
-        
-        return pt_dict
+import pandas as pd
+
+
 
 
 def parse_args(defaultdir):
@@ -28,12 +22,6 @@ def parse_args(defaultdir):
         Function to parse the arguments 
     """
     parser = ArgumentParser(description="Stepping through a pcl bag file to insert into a quadmap to save it.")
-    parser.add_argument("-lx", "--lowx", default=-40, help="lowest x value encountered in the pointcloud. Default is -40", type=int)
-    parser.add_argument("-ly", "--lowy", default=-40, help="lowest y value encountered in the pointcloud. Default is -40", type=int)
-    parser.add_argument("-sc", "--scale", default=100, help="scale of the Quadtree. Map extent will be low + scale. Default is 100", type=int)
-    parser.add_argument("--width", default=128, help="Image width. Default is 128. Change depending on network resolution", type=int)
-    parser.add_argument("--height", default=96, help="Image Height. default is 96. Change depending on network resolution", type=int)
-    parser.add_argument("-d", "--depth", default=16, help="depth of the quadtree. Default is 16.", type=int)
     parser.add_argument("--input", help="Input directory. If not specified, will raise FileNotFoundError", type=str)
     parser.add_argument("--output", help="Output directory. Default is ../output", default=defaultdir, type=str)
     parser.add_argument("--file", help="Input Filename. Used to read input from input directory and create equivalent output", type=str)
@@ -42,21 +30,29 @@ def parse_args(defaultdir):
     return args
 
 
+def find_boundaries(pts, min_x, min_y, max_x, max_y):
+    
+    for pt in pts:
+        if pt.x < min_x:
+            min_x = pt.x
+        elif pt.x > max_x:
+            max_x = pt.x
+        
+        if pt.y < min_y:
+            min_y = pt.y
+        elif pt.y > max_y:
+            max_y = pt.y
+    
+    return min_x, min_y, max_x, max_y
+
+
+
 if __name__=="__main__":
     dirname = os.path.dirname(os.path.abspath(__file__))
 
     # argument parsing
     defaultdir = os.path.join(dirname, '..', 'output')
     args = parse_args(defaultdir)
-
-    # Quadtree values
-    img_width = args.width
-    img_height = args.height
-    low = (args.lowx, args.lowy)
-    scale =  args.scale
-    max_depth = args.depth
-
-    tree = qt.Quadtree(low=low, scale=scale, max_depth=max_depth)
 
     # symlinked - may have to use "realpath" or something
     bagf = os.path.join(args.input, args.file + ".bag")
@@ -68,6 +64,8 @@ if __name__=="__main__":
     # timing setup
     timerlist = []
     hz = 30
+    min_x = min_y = 1000
+    max_x = max_y = -1000
     
     try:
         with rosbag.Bag(bagf, 'r') as bag:
@@ -76,18 +74,23 @@ if __name__=="__main__":
             for (topic, msg, ts) in tqdm(bag.read_messages(topics=pcl_topic), desc='progress', total=bag.get_message_count(topic_filters=pcl_topic)):
 
                 # Decoding the points
-                intensity_channel = msg.channels[1].values
                 pts = msg.points
-                val_dict = decode_intensity(pts, intensity_channel)
+                min_x, min_y, max_x, max_y = find_boundaries(pts, min_x, min_y, max_x, max_y)
                 
                 # Inserting the points
-                idcs = tree.insertion_idcs(pts, img_width, img_height)
-                tree.insert_points_arr(values=val_dict.values(), idcs = idcs)
 
         # output saving
         a = datetime.now()
         d = a.strftime("%y-%m-%d_%H-%M")
-        f = "{}_{}-qt-{}-{}-{}.pkl".format(d, args.file, max_depth, low, scale)
+        df = pd.DataFrame(columns=["min_x", "min_y", "max_x", "max_y"])
+        df.at[0, "min_x"] = min_x
+        df.at[0, "min_y"] = min_y
+        df.at[0, "max_x"] = max_x
+        df.at[0, "max_y"] = max_y
+        
+        # TODO: change this fileformat
+        f = "{}.csv".format(args.file)
         outpath = os.path.join(args.output, f)
-        tree.save(outpath)
+        df.to_csv(outpath)
+        print("Written csv to: {}".format(outpath))
     except FileNotFoundError: raise
