be9162b7d165ccdded7a0dc52ba06b7a1a3c9801
Nicolas Mandel <nicolasjohann.mandel@hdr.qut.edu.au>
2021-10-08 16:11:37 +1000

    added loop to job script bash file

--- src/job_script.sh
+++ src/job_script.sh
@@ -12,30 +12,37 @@ header_4="#PBS -l ncpus=1"
 # conda and directory
 body_1="conda activate robostackenv"
 body_2="cd ~/quadmap/src"
+py_file="dual-tree.py"
 
 # General stuff for the script
 # declare -a StringArray=("sim_hyb-10" "sim_hyb-20")
 search_dir=~/rosbag/pcl
 for m in "$search_dir"/*.bag 
 do
-	fname=$(basename "$m" .bag)
-	this_script_file="run_sim_${fname}.sh"
-	# HPC required header
-	echo ${header_1} >> ${this_script_file}
-	echo "#PBS -N ${fname}" >> ${this_script_file}
-	echo ${header_2} >> ${this_script_file}
-	echo ${header_3} >> ${this_script_file}
-	echo ${header_4} >> ${this_script_file}
-	
-	# body - loading conda and directory
-	echo ${body_1} >> ${this_script_file}
-	echo ${body_2} >> ${this_script_file}
-	
-	# Actual command to execute
-	echo "python bag-test.py --file ${fname} --output ../output --input ~/rosbag/pcl " >> ${this_script_file}
-	qsub ${this_script_file}
-	
-	#cleanup
-	rm ${this_script_file}
+	for i in {2..8}
+	do
+		fname=$(basename "$m" .bag)
+		this_script_file="run_sim_${fname}.sh"
+		# HPC required header
+		echo ${header_1} >> ${this_script_file}
+		echo "#PBS -N ${fname}" >> ${this_script_file}
+		echo ${header_2} >> ${this_script_file}
+		echo ${header_3} >> ${this_script_file}
+		echo ${header_4} >> ${this_script_file}
+		
+		# body - loading conda and directory
+		echo ${body_1} >> ${this_script_file}
+		echo ${body_2} >> ${this_script_file}
+		
+		# Actual command to execute
+		commandstring="python ${py_file} --file ${fname} --output ../output/21-10-08 --input ~/rosbag/pcl -r -cr ${i}"
+
+		echo ${commandstring} >> ${this_script_file}
+		echo "running: ${commandstring}"
+		qsub ${this_script_file}
+		
+		#cleanup
+		rm ${this_script_file}
+	done
 done
 
