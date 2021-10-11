b874d3f04f251ec53c87bc9d96a9fdcbf1a26b77
NicoMandel <nicolasjohann.mandel@hdr.qut.edu.au>
2021-10-11 13:53:17 +1000

    job-script reqritten to launch files with comparable tree dimensions

--- src/job_script.sh
+++ src/job_script.sh
@@ -2,7 +2,7 @@
 
 # These are the limits for the hpc script
 memory_limit="6000mb"
-time_limit="18:00:00"
+time_limit="20:00:00"
 header_1="#!/bin/bash -l"
 header_2="#PBS -l walltime=${time_limit}"
 header_3="#PBS -l mem=${memory_limit}"
@@ -12,21 +12,20 @@ header_4="#PBS -l ncpus=1"
 # conda and directory
 body_1="conda activate robostackenv"
 body_2="cd ~/quadmap/src"
-py_file="load_multi_tree.py"
+py_file="bag-test.py"
 
 # General stuff for the script
-declare -a StringArray=("sim_hyb-10" "sim_hyb-20" "sim_tgt1-ascend" "sim_tgt1-descend" "sim_tgt2-ascend" "sim_tgt2-descend" "sim_mission-10m" "sim_mission-20m" \
-"exp_tgt1-ascend" "exp_tgt1-descend" "exp_tgt2-ascend" "exp_tgt2-descend" "exp_hyb-freq-20m" "exp_mission-20m")
-numlist=(14 12 10 8 6 4 1)
-
-for e in "${StringArray[@]}" 
+# declare -a StringArray=("sim_hyb-10" "sim_hyb-20")
+search_dir=~/rosbag/pcl
+for m in "$search_dir"/*.bag 
 do
-	for d in ${numlist[@]}
+	for i in {2..8}
 	do
-		this_script_file="plot_${e}_${d}.sh"
+		fname=$(basename "$m" .bag)
+		this_script_file="run_sim_${fname}.sh"
 		# HPC required header
 		echo ${header_1} >> ${this_script_file}
-		echo "#PBS -N plot_${e}_${d}" >> ${this_script_file}
+		echo "#PBS -N ${fname}_${i}" >> ${this_script_file}
 		echo ${header_2} >> ${this_script_file}
 		echo ${header_3} >> ${this_script_file}
 		echo ${header_4} >> ${this_script_file}
@@ -36,7 +35,7 @@ do
 		echo ${body_2} >> ${this_script_file}
 		
 		# Actual command to execute
-		commandstring="python ${py_file} --input ../output/skips --file ${e} -d ${d} -s"
+		commandstring="python ${py_file} --file ${fname} --output ../output/21-10-11 --input ~/rosbag/pcl -r ${i} -t"
 
 		echo ${commandstring} >> ${this_script_file}
 		echo "running: ${commandstring}"
